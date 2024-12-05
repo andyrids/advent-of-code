cbeb8f78478772626717917b83f5d0997b939402
Andrew Ridyard <ridyard12@gmail.com>
2024-12-05 00:37:18 +0000

    feat: day 2 solution part 2

--- 2024/day_02/solution.py
+++ 2024/day_02/solution.py
@@ -4,15 +4,16 @@ import operator
 import pathlib
 
 
-def reduce_reports(accumulator: list, report: str) -> list[tuple]:
-    """
+def reduce_reports(accumulator: list, report: list[str]) -> list[list[int]]:
+    """Reduce reports into a list of lists, containing integers
+    representing levels.
 
     Args:
-        accumulator (list[tuple]): 
-        numbers (str): 
+        accumulator (list[list]): Levels in a list of lists.
+        report (list[str]): A list of numbers as a string.
 
     Returns:
-        
+        A list of lists containing integers.
     """
 
     accumulator.append(list(map(int, report)))
@@ -28,16 +29,32 @@ reports = functools.reduce(
 )
 
 
-def report_filter(report: list[int]) -> bool:
-    """
+def report_filter(report: list[int], dampener: bool = False) -> bool:
+    """Filter reports by checking if levels are increasing/decreasing
+    by 1-3. If dampener is True, we remove a level from a report list
+    and recursively check the levels again, until all iterations have
+    failed the check or an iteration passes. This represents tolerating
+    a single 'bad' level that prevents the increasing/decreasing by 1-3
+    check (part 2).
 
     Args:
-        report (list[int]):
+        report (list[int]): List of integers representing levels.
 
     Returns:
-        bool
+        True if a report levels meet the requirements else False.
     """
 
+    if dampener:
+        # indices in current report
+        indices = tuple(range(len(report)))
+        for idx in indices:
+            indexer = operator.itemgetter(*[i for i in indices if i != idx])
+            # temp report with all items except current idx (dampener usage)
+            temp_report = list(indexer(report))
+            # recursive check of temp report
+            if report_filter(temp_report):
+                return True
+        
     increasing = sorted(report, reverse=False)
     decreasing = sorted(report, reverse=True)
 
@@ -50,15 +67,22 @@ def report_filter(report: list[int]) -> bool:
             difference = decreasing.pop(0) - max(decreasing)
             if 0 < difference <= 3:
                 continue
-            # if level check fails
+            # if level check fails and no dampener
             return False
         # all checks passed
         return True
-    # report levels are not increasing or decreasing
+
+    # report levels are not increasing/decreasing & dampener failed
     return False
 
+print(f"Total reports: {len(reports)}")
 
+# part one safe reports
+safe_reports = tuple(filter(report_filter, reports))
+print(f"Safe reports (no dampener): {len(safe_reports)}")
 
+dampener_filter = functools.partial(report_filter, dampener=True)
+dampener_safe_reports = tuple(filter(dampener_filter, reports))
 
-print(len(tuple(filter(report_filter, reports))))
-
+# 321, 349, 297, 337
+print(f"Safe reports (dampener): {len(dampener_safe_reports)}")
