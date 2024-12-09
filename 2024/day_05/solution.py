e1271195f66b5b83a70cfe08689fbf64db2fd9de
Andrew Ridyard <ridyard12@gmail.com>
2024-12-09 21:58:14 +0000

    feat: day 05 part 2.

--- 2024/day_05/solution.py
+++ 2024/day_05/solution.py
@@ -11,6 +11,7 @@ input_txt = pathlib.Path(__file__).parent / "input.txt"
 
 rules, *_, pages = input_txt.read_text().partition("\n\n")
 
+incorrect_pages = []
 middle_number_total = 0
 for page in pages.splitlines():
     try:
@@ -21,11 +22,33 @@ for page in pages.splitlines():
                 if page.index(before) > page.index(after):
                     raise ValueError
     except ValueError:
+        incorrect_pages.append(page_numbers)
         continue
     else:
         middle = statistics.median(range(len(page_numbers)))
         middle_number_total += int(page_numbers[middle])
 
-           
 # 12667 (high) | 18300 (high) | 4185
 print(middle_number_total)
+
+
+def reorder_pages(pages: list[int]) -> list[int]:
+    """Reorder pages based on page order rules."""
+
+    changes = False
+    for rule in rules.splitlines():
+        before, after = rule.split("|")
+        if (before in pages) and (after in pages):
+            if (i := pages.index(before)) > (j := pages.index(after)):
+                pages[i], pages[j] = pages[j], pages[i]
+                changes = True
+    return changes
+
+
+middle_corrected_total = 0
+for pages in incorrect_pages:
+    while reorder_pages(pages):
+        pass
+    middle_corrected_total += int(pages[len(pages) // 2])
+
+print(middle_corrected_total)
